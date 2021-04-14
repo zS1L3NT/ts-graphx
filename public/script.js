8376625b045d85ffb179516a60dedfcb427ce875
Zechariah <zechariahtan144@gmail.com>
2021-04-14 23:45:06 +0800

    Bare basics, graph works, needs polishing

--- public/script.js
+++ public/script.js
@@ -2,15 +2,51 @@ window.onload = () => {
 	const app = new Vue({
 		el: "#app",
 		data: {
-			equation: ""
+			equation: "",
+			coordinates: [],
+			error: "",
+			rangeX: {
+				low: -5,
+				high: 5
+			},
+			rangeY: {
+				low: -5,
+				high: 5
+			}
 		},
 		methods: {
 			calculate: function () {
 				axios
-					.post("/", { data: this.equation })
-					.then(console.log)
-					.catch(console.error)
+					.post("/", {
+						data: this.equation,
+						rangeX: this.rangeX,
+						rangeY: this.rangeY,
+						step: this.stepX
+					})
+					.then((res) => {
+						this.coordinates = res.data
+						this.error = ""
+					})
+					.catch((e) => {
+						console.log(e)
+						this.coordinates = []
+						this.error = e.response.data
+						snackbar.open()
+					})
+			}
+		},
+		computed: {
+			stepX: function () {
+				return (this.rangeX.high - this.rangeX.low) / 500
+			},
+			stepY: function () {
+				return (this.rangeY.high - this.rangeY.low) / 500
 			}
 		}
 	})
+
+	const snackbar = new mdc.snackbar.MDCSnackbar(
+		document.querySelector(".error")
+	)
+	new mdc.textField.MDCTextField(document.querySelector(".equation"))
 }
